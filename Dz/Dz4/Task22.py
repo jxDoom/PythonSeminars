f7ab415c016787c957286ab770a85bf2766d1ece
Alexey Amanov <chelsea.alex@list.ru>
2023-06-05 23:21:27 +0300

    updated task 22 and added task 24

--- Dz/Dz4/Task22.py
+++ Dz/Dz4/Task22.py
@@ -3,3 +3,23 @@
 # Пользователь вводит 2 числа. n — кол-во элементов первого множества.
 # m — кол-во элементов второго множества. Затем пользователь вводит сами элементы множеств.
 
+import random
+
+a = int(input('Initial value number: '))
+b = int(input('Final value number: '))
+
+if a >= 0 and b >= 0 or a < 0 and b < 0:
+    size_n = int(input('Enter size first set = '))
+    size_m = int(input('Enter size second set = '))
+
+    n = [random.randint(a, b) for i in range(size_n)]
+    print(n)
+    n = set(n)
+
+    m = [random.randint(a, b) for i in range(size_m)]
+    print(m)
+    m = set(m)
+
+    print(n.union(m))
+else:
+    print('Error: re-enter numbers')
