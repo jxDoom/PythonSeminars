d862054e0ee6e5e03ff54548d44ef445f977c5a8
Alexey Amanov <chelsea.alex@list.ru>
2023-07-24 01:29:24 +0300

    updated De Morgan

--- Dz/Dz2/Task2-DeMorgan.py
+++ Dz/Dz2/Task2-DeMorgan.py
@@ -6,15 +6,18 @@
 # Предикаты НЕ ЗАДАЕМ как целое число!
 
 from random import choice
-
+from time import time
+start = time()
 #sp = ['X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+for _ in range(100):
+    x = choice([True, False])
+    y = choice([True, False])
+    z = choice([True, False])
+    #print(('X = {}, Y = {}, Z = {}').format(x, y, z))
 
-x = choice([True, False])
-y = choice([True, False])
-z = choice([True, False])
-print(('X = {}, Y = {}, Z = {}').format(x, y, z))
-
-if not(x or y or z) == (not x and not y and not z):
-    print('true')
-else:
-    print('false')
+    if not(x or y or z) == (not x and not y and not z):
+        print(' ', end = '')
+    else:
+        print('false')
+print()
+print(time()-start)
