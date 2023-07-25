1bb7e00b941da297a0c0369e4823948dda9b4d4b
Alexey Amanov <chelsea.alex@list.ru>
2023-07-25 22:59:26 +0300

    corrected solution

--- Dz/Dz2/Task2-DeMorgan.py
+++ Dz/Dz2/Task2-DeMorgan.py
@@ -8,28 +8,25 @@
 from random import choice, randint
 from time import time
 start = time()
+k = 0
 
-for _ in range(100):
-    sp = ['x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
-    new_sp = list()
-    for i in range(randint(3, 15)):
-        sp[i] = choice([True, False])
-        new_sp.append(sp[i])
-    
-    #print(new_sp)
+while(k < 100):
+    sp = [choice([True, False]) for i in range(randint(3, 15))]
+    # print(sp)
     left_side = False
     right_side = True
     
-    for i in range(len(new_sp)):
-        tmp_left = new_sp[i]
+    for i in range(len(sp)):
+        tmp_left = sp[i]
         left_side = left_side or tmp_left
-        tmp_right = new_sp[i]
+        tmp_right = sp[i]
         right_side = right_side and not tmp_right
 
     if not left_side == right_side:
         print('Statement is true')
     else:
         print('Statement is false')
+    k += 1
 
 end = time()-start
 print(end)
@@ -55,3 +52,34 @@ print(end)
 #         print(f'{item} = {choice([True, False])}')
         
 # func('x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l')
+
+# ------------------------------------------------------------------------------------------------------------
+"""
+from random import *
+# x = choice([True,False])
+# y = choice([True,False])
+# z = choice([True,False])
+
+def predicate_generator(predicate_count):
+    predicates = []
+    for i in range(predicate_count):
+        predicates.append(choice([True,False]))
+    return predicates
+
+
+
+def de_morgan(predicates):
+    left_side = predicates[0]
+    right_side = not predicates[0]
+    for i in range(1,len(predicates)):
+        left_side = left_side or predicates[i]
+        right_side = right_side and not predicates[i]
+    return not left_side == right_side       
+        
+
+j = 0
+while(j < 100):
+    count_predicate = randint(3,15)
+    print(de_morgan(predicate_generator(count_predicate)))
+    j += 1
+"""
