1fa57a6d92be621f81d3df5f10519676e119b913
Alexey Amanov <chelsea.alex@list.ru>
2023-08-01 19:14:01 +0300

    completed task

--- Dz/Dz4/Task2-SumPolinom.py
+++ Dz/Dz4/Task2-SumPolinom.py
@@ -16,19 +16,52 @@ def completingDict(exponent):
     dictionary = dict(array)
     return dictionary
 
+def priorityDict(dict_max, dictionary):
+    for key, value in dictionary.items():
+        if key in dict_max:
+            dict_max[key] += value
+        else:
+            dict_max = value
+    return dict_max
+
+def equationPrint(dictionary):
+    res = ''
+    for key, value in dictionary.items():    
+        if value == 0:
+            res += ''
+        elif value == -1 and key == 'x^0':
+            res += '-1 + '
+        elif value == 1 and key == 'x^0':
+            res += '1 + '
+        elif value == -1 and key == 'x^1':
+            res += '-x + '
+        elif value == 1 and key == 'x^1':
+            res += 'x + '
+        elif value == -1:
+            res += f'-{key} + '
+        elif value == 1:
+            res += f'{key} + '
+        elif key == 'x^0':
+            res += f'{value} + '
+        elif key == 'x^1':
+            res += f'{value}x + '
+        else:
+            res += f'{value}{key} + '
+    
+    res = res[:-3].replace('+ -', '- ')
+    return res
+
 exp_max1: int = int(input('Введите максимальную натуральную степень первого многочлена: '))
-exp_max2: int = int(input('Введите максимальную натуральную степень второго многочлена: '))
 
 dict_plm1 = completingDict(exp_max1)
-print('Первый многочлен:\n', dict_plm1)
+print('Первый многочлен:\n', equationPrint(dict_plm1), '= 0')
 print()
+exp_max2: int = int(input('Введите максимальную натуральную степень второго многочлена: '))
 dict_plm2 = completingDict(exp_max2)
-print('Второй многочлен:\n', dict_plm2)
+print('Второй многочлен:\n', equationPrint(dict_plm2), '= 0')
+print()
 
-# for key, value in dict_plm2.items():
-#     if key in dict_plm1:
-#         dict_plm1[key].extend(value)
-#     else:
-#         dict_plm1 = value
-
-print(dict_plm1)
+if len(dict_plm2) > len(dict_plm1):
+    print('Результирующий многочлен:\n', equationPrint(priorityDict(dict_plm2, dict_plm1)), '= 0')
+else:
+    print('Результирующий многочлен:\n', equationPrint(priorityDict(dict_plm1, dict_plm2)), '= 0')
