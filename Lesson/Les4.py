36da6e4eec92f0593da0cbc5df644eb8855ccb75
Alexey Amanov <chelsea.alex@list.ru>
2023-08-03 00:20:52 +0300

    added map()

--- Lesson/Les4.py
+++ Lesson/Les4.py
@@ -54,3 +54,32 @@
 # print(res)
 # res = select(lambda x: (x, x**2), res)
 # print(res)
+
+# -----------------------------------------------------------------------
+# Функция map()
+# list_1 = [x for x in range(1, 20)]
+# print(list_1)
+
+# list_1 = list(map(lambda x: x + 10, list_1))
+# print(list_1)
+
+# Задача: C клавиатуры вводится некий набор чисел, в качестве разделителя используется
+# пробел. Этот набор чисел будет считан в качестве строки. Как превратить list строк в list чисел?
+
+# data = '15 156 96 3 5 8 52 5'
+# data = list(map(int, data.split()))
+# print(data)
+
+
+# def where(f, col):
+#     return [x for x in col if f(x)]
+
+# sp = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = list(map(int, sp))
+# print(res)
+# res = where(lambda x: x % 2 == 0, sp)
+# print(res)
+# res = list(map(lambda x: (x, x**2), res))
+# print(res)
+
+# Функция filter
