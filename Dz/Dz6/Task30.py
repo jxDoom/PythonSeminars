a9c56d816a8095d6c10e7670cb5a3b39dd6b7f71
Alexey Amanov <chelsea.alex@list.ru>
2023-08-06 18:25:37 +0300

    task 30 solved

---
+++ Dz/Dz6/Task30.py
@@ -0,0 +1,18 @@
+# Заполните массив элементами арифметической прогрессии.
+# Её первый элемент, разность и количество элементов нужно ввести с клавиатуры.
+# Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+
+# def ArithProg(first_num, diff, count):    #Первый элемент, разность и кол-во элементов соответственно
+#     sp = [first_num + (i - 1) * diff for i in range(1, count + 1)]
+#     return sp
+
+def Start():
+    a1: int = int(input('Введите первый элемент арифметической прогрессии: '))
+    d: int = int(input('Разность элементов: '))
+    n: int = int(input('Количество элементов: '))
+    if d > 0 and n > 1:
+        ArithProg = lambda first_num, diff, count: [first_num + (i - 1) * diff for i in range(1, count + 1)]
+    print(ArithProg(a1, d, n))
+
+Start()
