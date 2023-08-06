cc645312ac97bbc1c25981c53ca2b813de885a68
Alexey Amanov <chelsea.alex@list.ru>
2023-08-06 19:17:34 +0300

    task 32 solved

--- Dz/Dz6/Task32.py
+++ Dz/Dz6/Task32.py
@@ -6,7 +6,29 @@
 # 200
 # Ответ: [2, 3]
 
-# sp = [ 1, 5, 88, 100, 2, -4]
-# n_min = 33
-# n_max = 200
-# for i in range(sp):
+from random import randint
+
+ListGenerate = lambda size: [randint(-100, 100) for i in range(size)]
+
+def FindIndex(spisok, min_num, max_num):
+    index = []
+    for item, element in enumerate(spisok):
+        if element > min_num and element < max_num:    #min_num < element < max_num
+            index.append(item)
+    return index
+
+def Start():
+    size: int = int(input('Определите размер массива: '))
+    if size > 0:
+        sp = ListGenerate(size)
+        print(sp)
+        #minimum: int = int(input('Заданный минимум = '))
+        #maximum: int = int(input('Заданный максимум = '))
+        minimum = -25
+        maximum = 40
+        if maximum > minimum and maximum - minimum > 1:
+            print(FindIndex(sp, minimum, maximum))
+        else: print(-1)
+    else: print(0)
+
+Start()
