dc7e5a1cdaa8e6b5b3750e0d2d11744ae0b44714
Alexey Amanov <chelsea.alex@list.ru>
2023-08-06 18:31:55 +0300

    task 30 solved

--- Dz/Dz6/Task30.py
+++ Dz/Dz6/Task30.py
@@ -8,11 +8,13 @@
 #     return sp
 
 def Start():
+    ArithProg = lambda first_num, diff, count: [first_num + (i - 1) * diff for i in range(1, count + 1)]
     a1: int = int(input('Введите первый элемент арифметической прогрессии: '))
     d: int = int(input('Разность элементов: '))
     n: int = int(input('Количество элементов: '))
     if d > 0 and n > 1:
-        ArithProg = lambda first_num, diff, count: [first_num + (i - 1) * diff for i in range(1, count + 1)]
-    print(ArithProg(a1, d, n))
-
+        print(ArithProg(a1, d, n))
+    else:
+        print(0)
+    
 Start()
