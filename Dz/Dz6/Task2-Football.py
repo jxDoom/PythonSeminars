dddeff82d0d73e640da267d19390285b1d757be7
Alexey Amanov <chelsea.alex@list.ru>
2023-08-08 01:07:10 +0300

    added extra task unfinished

--- Dz/Dz6/Task2-Football.py
+++ Dz/Dz6/Task2-Football.py
@@ -27,3 +27,32 @@
 # Спартак:2 0 0 2 0
 # Зенит:2 1 0 1 3
 # Локомотив:2 2 0 0 6
+from random import randint
+
+teams = ['Chelsea', 'Man City', 'Arsenal', 'Man United', 'Barcelona']#,
+         #'Real Madrid', 'Liverpool', 'Bayern', 'PSJ', 'Newcastle']
+#[Games, Result, Points]    #Result: return 0 0 0
+
+def GenerateMatch(team):
+    for i in range(len(team)):
+        index = i - len(team)
+        index1 = index2 = index
+        team1 = team[index1]
+        while index2 < i - 1:
+            team2 = team[index2]
+            index2 += 1
+            left = randint(0, 5)
+            right = randint(0, 5)
+            print(f'{team[index1]} {left} : {right} {team[index2]}')
+
+# def Games(team):
+#     count = 0
+#     if 
+GenerateMatch(teams)
+
+# for i in range(len(team)):
+#     index1 = i - len(team)
+#     team1 = team[index1]
+#     index2 = i - len(team) + 1
+#     team2 = team[index2]
+#     print(f'{team[index1]} {randint(0, 5)} : {randint(0, 5)} {team[index2]}')
