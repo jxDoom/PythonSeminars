a391db317ffd2a4219d407918bcd7a4961b6c5d7
Alexey Amanov <chelsea.alex@list.ru>
2023-08-10 01:10:26 +0300

    almost ready

--- Dz/Dz6/Task2-Football.py
+++ Dz/Dz6/Task2-Football.py
@@ -27,32 +27,54 @@
 # Спартак:2 0 0 2 0
 # Зенит:2 1 0 1 3
 # Локомотив:2 2 0 0 6
-from random import randint
 
-teams = ['Chelsea', 'Man City', 'Arsenal', 'Man United', 'Barcelona']#,
-         #'Real Madrid', 'Liverpool', 'Bayern', 'PSJ', 'Newcastle']
-#[Games, Result, Points]    #Result: return 0 0 0
+from random import randint, sample
+from operator import itemgetter
 
-def GenerateMatch(team):
-    for i in range(len(team)):
-        index = i - len(team)
-        index1 = index2 = index
-        team1 = team[index1]
-        while index2 < i - 1:
-            team2 = team[index2]
-            index2 += 1
-            left = randint(0, 5)
-            right = randint(0, 5)
-            print(f'{team[index1]} {left} : {right} {team[index2]}')
+def newUpdate(oldValue, newValue):
+    res = oldValue.copy()
+    for i in range(5):
+        res[i] += newValue[i]
+    return res
 
-# def Games(team):
-#     count = 0
-#     if 
-GenerateMatch(teams)
+teams = ['Chelsea', 'Man City', 'Arsenal', 'Man United', 'Barcelona',
+        'Real Madrid', 'Liverpool', 'Bayern', 'PSJ', 'Newcastle']
 
-# for i in range(len(team)):
-#     index1 = i - len(team)
-#     team1 = team[index1]
-#     index2 = i - len(team) + 1
-#     team2 = team[index2]
-#     print(f'{team[index1]} {randint(0, 5)} : {randint(0, 5)} {team[index2]}')
+size: int = int(input('Enter count of clubs (min 2, max 10) = '))
+team = sample(teams, size)
+
+general_table = dict.fromkeys(team, [0, 0, 0, 0, 0])
+
+for i in range(len(team)):
+    index = i - len(team)
+    index1 = index2 = index
+    team1 = team[index1]
+    while index2 < i - 1:
+        team2 = team[index2]
+        index2 += 1
+        left = randint(0, 5)
+        right = randint(0, 5)
+        print(f'{team[index1]} {left} : {right} {team[index2]}')
+        if left > right:
+            tmp = newUpdate(general_table[team[index1]], [1, 1, 0, 0, 3])
+            general_table.update({team[index1]: tmp})
+
+            tmp = newUpdate(general_table[team[index2]], [1, 0, 0, 1, 0])
+            general_table.update({team[index2]: tmp})
+        elif left < right:
+            tmp = newUpdate(general_table[team[index1]], [1, 0, 0, 1, 0])
+            general_table.update({team[index1]: tmp})
+
+            tmp = newUpdate(general_table[team[index2]], [1, 1, 0, 0, 3])
+            general_table.update({team[index2]: tmp})
+        else:
+            tmp = newUpdate(general_table[team[index1]], [1, 0, 1, 0, 1])
+            general_table.update({team[index1]: tmp})
+
+            tmp = newUpdate(general_table[team[index2]], [1, 0, 1, 0, 1])
+            general_table.update({team[index2]: tmp})
+
+print()        
+for key, value in general_table.items():
+    print(f'{key}:', *value)
+
