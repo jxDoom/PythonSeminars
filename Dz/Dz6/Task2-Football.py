98116c0290507f5ccb3f1be767b6c01b5c856c30
Alexey Amanov <chelsea.alex@list.ru>
2023-08-10 21:11:30 +0300

    task solved

--- Dz/Dz6/Task2-Football.py
+++ Dz/Dz6/Task2-Football.py
@@ -29,52 +29,59 @@
 # Локомотив:2 2 0 0 6
 
 from random import randint, sample
-from operator import itemgetter
 
-def newUpdate(oldValue, newValue):
+def NewUpdate(oldValue, newValue):
     res = oldValue.copy()
     for i in range(5):
         res[i] += newValue[i]
     return res
 
-teams = ['Chelsea', 'Man City', 'Arsenal', 'Man United', 'Barcelona',
-        'Real Madrid', 'Liverpool', 'Bayern', 'PSJ', 'Newcastle']
+def Condition(table, team1, team2, goals_team1, goals_team2):
+    if goals_team1 > goals_team2:
+        tmp = NewUpdate(table[team1], [1, 1, 0, 0, 3])
+        table.update({team1: tmp})
 
-size: int = int(input('Enter count of clubs (min 2, max 10) = '))
-team = sample(teams, size)
+        tmp = NewUpdate(table[team2], [1, 0, 0, 1, 0])
+        table.update({team2: tmp})
+    elif goals_team1 < goals_team2:
+        tmp = NewUpdate(table[team1], [1, 0, 0, 1, 0])
+        table.update({team1: tmp})
 
-general_table = dict.fromkeys(team, [0, 0, 0, 0, 0])
+        tmp = NewUpdate(table[team2], [1, 1, 0, 0, 3])
+        table.update({team2: tmp})
+    else:
+        tmp = NewUpdate(table[team1], [1, 0, 1, 0, 1])
+        table.update({team1: tmp})
 
-for i in range(len(team)):
-    index = i - len(team)
-    index1 = index2 = index
-    team1 = team[index1]
-    while index2 < i - 1:
-        team2 = team[index2]
-        index2 += 1
-        left = randint(0, 5)
-        right = randint(0, 5)
-        print(f'{team[index1]} {left} : {right} {team[index2]}')
-        if left > right:
-            tmp = newUpdate(general_table[team[index1]], [1, 1, 0, 0, 3])
-            general_table.update({team[index1]: tmp})
+        tmp = NewUpdate(table[team2], [1, 0, 1, 0, 1])
+        table.update({team2: tmp})
+    return table
 
-            tmp = newUpdate(general_table[team[index2]], [1, 0, 0, 1, 0])
-            general_table.update({team[index2]: tmp})
-        elif left < right:
-            tmp = newUpdate(general_table[team[index1]], [1, 0, 0, 1, 0])
-            general_table.update({team[index1]: tmp})
+def Results(team, table):
+    # for index1 in range(len(team) - 1):               #одноматчевые встречи
+        # for index2 in range(index1 + 1, len(team)):
+    for i in range(len(team)):                          #двухматчевые (дома и в гостях)
+        index = i - len(team)
+        index1 = index2 = index
+        team1 = team[index1]
+        while index2 < i - 1:
+            team2 = team[index2]
+            index2 += 1                                 # -----------------------------
+            left = randint(0, 5)
+            right = randint(0, 5)
+            print(f'{team[index1]} {left} : {right} {team[index2]}')
+            Condition(table, team[index1], team[index2], left, right)
 
-            tmp = newUpdate(general_table[team[index2]], [1, 1, 0, 0, 3])
-            general_table.update({team[index2]: tmp})
-        else:
-            tmp = newUpdate(general_table[team[index1]], [1, 0, 1, 0, 1])
-            general_table.update({team[index1]: tmp})
+def Start():
+    teams = ['Chelsea', 'Man City', 'Arsenal', 'Man United', 'Barcelona',
+            'Real Madrid', 'Liverpool', 'Bayern', 'PSJ', 'Newcastle']
 
-            tmp = newUpdate(general_table[team[index2]], [1, 0, 1, 0, 1])
-            general_table.update({team[index2]: tmp})
+    size: int = int(input('Enter count of clubs (min 2, max 10) = '))
+    team = sample(teams, size)
+    general_table = dict.fromkeys(team, [0, 0, 0, 0, 0])
+    Results(team, general_table)
+    print()        
+    for key, value in general_table.items():
+        print(f'{key}:', *value)
 
-print()        
-for key, value in general_table.items():
-    print(f'{key}:', *value)
-
+Start()
